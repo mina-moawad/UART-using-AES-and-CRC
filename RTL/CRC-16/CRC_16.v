77844895291611b9206237e95446e77b3d6dda48
hoda404ayman <hodaosman3124@gmail.com>
2026-09-03 18:00:28 +0300

    Adding CRC first version - needs multi frame handling and 128 bit data scaling

---
+++ RTL/CRC-16/CRC_16.v
@@ -0,0 +1,118 @@
+module CRC #(
+    parameter DATA_WIDTH = 4,
+    parameter CRC_WIDTH  = 3,
+
+    // Polynomial has CRC_WIDTH + 1 bits
+    parameter [CRC_WIDTH:0] POLYNOMIAL = 4'b1011
+
+)(
+    
+    input  wire [DATA_WIDTH-1:0] data_in,
+
+    input  wire                  clk,
+    input  wire                  rst_n,
+
+    output reg [DATA_WIDTH+CRC_WIDTH-1:0] data_with_crc,
+    output reg                  done
+
+);
+
+
+    reg [CRC_WIDTH:0] stage;   
+    // reg [CRC_WIDTH:0] stage_2;        
+     
+
+    reg [DATA_WIDTH+CRC_WIDTH-1:0] data_with_zeros;
+
+    reg [DATA_WIDTH-1:0] counter;
+
+
+
+    always @(posedge clk or negedge rst_n) begin
+
+        if (!rst_n) begin
+
+            stage          <= {(CRC_WIDTH+1){1'b0}};
+            // stage_2        <= {(CRC_WIDTH+1){1'b0}};
+            data_with_zeros <= {(DATA_WIDTH+CRC_WIDTH){1'b0}};
+            counter        <= {DATA_WIDTH{1'b0}};
+            data_with_crc  <= {(DATA_WIDTH+CRC_WIDTH){1'b0}};
+            done           <= 1'b0;
+
+        end
+
+        else begin
+            done <= 1'b0;
+
+
+            if (counter == 0) begin
+
+                data_with_zeros <= {
+                    data_in,
+                    {CRC_WIDTH{1'b0}}
+                };
+
+                stage <= data_in[DATA_WIDTH-1:
+                                  DATA_WIDTH-CRC_WIDTH-1];
+
+                counter <= counter + 1'b1;
+
+            end
+
+
+            
+
+            else if (counter < DATA_WIDTH) begin
+
+                if (stage[CRC_WIDTH]) begin
+                    stage <= {
+                        stage[CRC_WIDTH-1:0] ^ POLYNOMIAL[CRC_WIDTH-1:0],
+                        data_with_zeros[CRC_WIDTH-counter]
+                    }  ;
+
+                end
+
+                else begin
+                    stage <= {
+                        stage[CRC_WIDTH-1:0],
+                        data_with_zeros[ CRC_WIDTH-counter]
+                    };
+
+                end
+
+                counter <= counter + 1'b1;
+
+            end
+
+
+    
+
+            else if (counter == DATA_WIDTH) begin
+
+                if (stage[CRC_WIDTH]) begin
+                    data_with_crc <= {
+                        data_in,
+                        stage[CRC_WIDTH-1:0] ^ POLYNOMIAL[CRC_WIDTH-1:0]
+                    };
+
+                end
+
+                else begin
+                    data_with_crc <= {
+                        data_in,
+                        stage[CRC_WIDTH-1:0]
+                    };
+
+                end
+                done <= 1'b1;
+
+                counter  <= 0 ; 
+                stage    <= 0 ;
+
+            end
+        
+        end
+
+    end
+
+endmodule
---
+++ RTL/CRC-16/CRC_16_tb.v
@@ -0,0 +1,137 @@
+`timescale 1ns/1ps
+module CRC_tb;
+
+    // -----------------------------------------
+    // Parameters
+    // -----------------------------------------
+
+    parameter DATA_WIDTH = 4;
+    parameter CRC_WIDTH  = 3;
+
+    // -----------------------------------------
+    // Testbench signals
+    // -----------------------------------------
+
+    reg [DATA_WIDTH-1:0] data_in;
+    reg                  clk;
+    reg                  rst_n;
+
+    wire [DATA_WIDTH+CRC_WIDTH-1:0] data_with_crc;
+    wire                             done;
+
+    // -----------------------------------------
+    // DUT
+    // -----------------------------------------
+
+    CRC #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .CRC_WIDTH(CRC_WIDTH),
+        .POLYNOMIAL(4'b1011)
+    )
+    DUT (
+        .data_in(data_in),
+        .clk(clk),
+        .rst_n(rst_n),
+        .data_with_crc(data_with_crc),
+        .done(done)
+        );
+
+
+    // -----------------------------------------
+    // Clock
+    // 10 ns period
+    // -----------------------------------------
+
+    always #5 clk = ~clk;
+
+
+    // -----------------------------------------
+    // Test
+    // -----------------------------------------
+
+    initial begin
+
+        // Initial values
+        clk    = 1'b0;
+        rst_n  = 1'b0;
+        data_in = 4'b0000;
+
+        // Reset
+        #20;
+        rst_n = 1'b1;
+        
+
+        // =====================================
+        // TEST 1
+        // =====================================
+
+        data_in = 4'b1001;
+
+        // Wait until CRC calculation finishes
+        wait(done);
+
+       #2;
+
+        $display("Data = %b | Data + CRC = %b",
+                 data_in,
+                 data_with_crc);
+
+
+        // =====================================
+        // TEST 2
+        // =====================================
+
+        #10;
+
+        data_in = 4'b1010;
+
+        wait(done);
+
+        #2;
+
+        $display("Data = %b | Data + CRC = %b",
+                 data_in,
+                 data_with_crc);
+
+
+        // =====================================
+        // TEST 3
+        // =====================================
+
+        #10;
+
+        data_in = 4'b0000;
+
+        wait(done);
+
+        #2;
+
+        $display("Data = %b | Data + CRC = %b",
+                 data_in,
+                 data_with_crc);
+
+
+        #20;
+
+        $finish;
+
+    end
+
+
+    // -----------------------------------------
+    // Monitor
+    // -----------------------------------------
+
+    initial begin
+
+        $monitor("Time=%0t | clk=%b | rst_n=%b | data=%b | CRC_word=%b | done=%b",
+                 $time,
+                 clk,
+                 rst_n,
+                 data_in,
+                 data_with_crc,
+                 done);
+
+    end
+
+endmodule
